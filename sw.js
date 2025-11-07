6ada6f6a95e92cd22be0b97e37ad323644567aee
saeed saffari <www.44web.ir@gmail.com>
2025-11-07 19:12:08 +0330

    sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,37 @@
-const CACHE_NAME = 'cst-pwa-v1';
-const urlsToCache = [
-  '/cst/',
-  '/cst/index.html',
-  '/cst/manifest.json'
-];
-
-self.addEventListener('install', (event) => {
+// نصب
+self.addEventListener('install', event => {
+  console.log('Service Worker نصب شد ✅');
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open('pwa-global-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icons/icon-192x192.png',
+        './icons/icon-512x512.png'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+// فعال شدن
+self.addEventListener('activate', event => {
+  console.log('Service Worker فعال شد 🔥');
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== 'pwa-global-v1') return caches.delete(key);
+      }));
+    })
+  );
+});
+
+// واکنش به درخواست‌ها
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
