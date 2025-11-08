8c52583ee09ff54cb2e9518e8ac0c5a4fbba7cc7
saeed saffari <www.44web.ir@gmail.com>
2025-11-08 08:10:13 +0330

    sw.js

--- sw.js
+++ sw.js
@@ -18,4 +18,11 @@
       "type": "image/png"
     }
   ]
+
+  if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/cst/sw.js')
+    .then(function(reg) { console.log('Registered', reg); })
+    .catch(function(err) { console.error('SW registration failed', err); });
+}
+
 }
