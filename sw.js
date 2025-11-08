4a1bd852b5c229cdfd4fb41c32c943e2d53cb3b4
saeed saffari <www.44web.ir@gmail.com>
2025-11-08 08:18:10 +0330

    sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,8 @@
-{
-  "name": "ربات CST",
-  "short_name": "CST",
-  "start_url": ".",
-  "display": "standalone",
-  "background_color": "#6a11cb",
-  "theme_color": "#2575fc",
-  "orientation": "portrait",
-  "icons": [
-    {
-      "src": "icons/icon-192x192.png",
-      "sizes": "192x192",
-      "type": "image/png"
-    },
-    {
-      "src": "icons/icon-512x512.png",
-      "sizes": "512x512",
-      "type": "image/png"
-    }
-  ]
+self.addEventListener('install', e => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
 
-  if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/cst/sw.js')
-    .then(function(reg) { console.log('Registered', reg); })
-    .catch(function(err) { console.error('SW registration failed', err); });
-}
-
-}
+self.addEventListener('fetch', e => {
+  // فقط برای فعال بودن تست
+});
